6ce8c069a6181c50ad41664f37b735d777d79d67
Adam School <adrodgers@my365.bellevue.edu>
2021-12-12 19:42:39 -0600

    WEB335 Week 8

---
+++ week-8/rodgers_calculator.py
@@ -0,0 +1,17 @@
+# Adam Rodgers
+# WEB335
+# Assignment 8.3
+# 12/12/2021
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num3, num4):
+    return num3 - num4
+
+def divide(num5, num6):
+    return num5/num6
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
